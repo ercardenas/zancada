9d91dd393147e7d98f90a57d89f217a01549d55f
Erick Cardenas <ecardenas@utexas.edu>
2024-05-29 15:07:59 -0700

    Set up the version catalog

--- app/src/main/java/com/zancada/MainActivity.kt
+++ app/src/main/java/com/zancada/MainActivity.kt
@@ -3,28 +3,16 @@ package com.zancada
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.zancada.ui.theme.ZancadaTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ZancadaTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            Greeting("Android")
         }
     }
 }
@@ -40,7 +28,5 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
-    ZancadaTheme {
-        Greeting("Android")
-    }
+    Greeting("Android")
 }

73bdd0fd5f5866df05655660e26e474fcb544ba1
Erick Cardenas <ecardenas@utexas.edu>
2024-05-31 13:43:29 -0700

    Set up AndroidApplicationConventionPlugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,20 +1,12 @@
 plugins {
-    alias(libs.plugins.androidApplication)
-    alias(libs.plugins.jetbrainsKotlinAndroid)
     alias(libs.plugins.mapsplatform.secrets.plugin)
+    alias(libs.plugins.zancada.android.application)
 }
 
 android {
     namespace = "com.zancada"
-    compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.zancada"
-        minSdk = 24
-        targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
-
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
@@ -29,13 +21,6 @@ android {
                 "proguard-rules.pro"
             )
         }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
-    }
-    kotlinOptions {
-        jvmTarget = "1.8"
     }
     buildFeatures {
         compose = true
--- build-logic/convention/src/main/java/com/zancada/convention/MyClass.kt
+++
@@ -1,4 +0,0 @@
-package com.zancada.convention
-
-class MyClass {
-}
